fd76fc4cc138f4744188b4af42b233897208d3ec
Abhinav Rastogi <abhinav.rastogi@harness.io>
2025-06-16 13:06:46 -0700

    Update template.ts to include Layout schema
    Adds an optional Layout schema for controlling the UI ordering and grouping of inputs

--- schema/template.ts
+++ schema/template.ts
@@ -10,4 +10,20 @@ export interface Template {
     inputs?: Record<string, Input>
     stage?: Stage;
     step?: Step;
+    /**
+     * Provide a Layout to control ordering and grouping of inputs in the UI
+     */
+    layout?: Layout
+}
+
+type Layout = Array<InputName | InputGroup>
+type InputName = string
+
+/*
+ * InputGroups will be shown as accordions in the UI if a title is provided
+ */
+interface InputGroup {
+  items: Layout // you can have nested groups within groups
+  title?: string
+  open?: boolean
 }
